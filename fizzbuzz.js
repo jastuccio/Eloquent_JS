eea86f5a187b8cf70fb41b24f5b83494455463ff
joe <joe@astucc.io>
2017-02-07 18:07:16 -0500

    FizzBuzz solved

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -4,11 +4,11 @@ Write a program that uses console.log to print all the numbers from 1 to 100, wi
 
 (function(){
 
-  for (i = 0; i < 100; i++) {
-  	console.log(i);
-  }	
-
-	
-
+	for (i = 1; i < 100; i++) {
+		var result = "";
+	    if (i % 3 === 0) { result += "Fizz";}
+	    if (i % 5 === 0) { result += "Buzz";}
+	  	console.log(result || i);
+	}	
 	
 }());
