4d190707ce4f0dd047d6058c9647a90944b43086
joe <joe@astucc.io>
2017-02-07 17:59:41 -0500

    Initial commit

---
+++ looping_triangle.js
@@ -0,0 +1,10 @@
+/*Write a loop that makes seven calls to console.log to output the following triangle:
+*/
+
+(function(){
+	var triangle = '';
+	for (i = 0; triangle.length < 7; i++) {
+		triangle += '#';
+		console.log(triangle);
+	}
+}());
