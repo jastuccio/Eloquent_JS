3b07bf835a8cafd16b6cc96b973cf9a95f4676e5
joe <joe@astucc.io>
2017-02-07 21:53:49 -0500

    solve Ch3 minimum

--- 03_functions/minimum.js
+++ 03_functions/minimum.js
@@ -1,34 +1,17 @@
-/*
+/* Write a function min that takes two arguments and returns their minimum. */
 
-Write a program that creates a string that represents an 8×8 grid, using newline characters to separate lines. At each position of the grid there is either a space or a “#” character. The characters should form a chess board.
+(function(){
 
-Passing this string to console.log should show something like this:
+	var result;
+	var min = function (a, b) {
+		var result = a;
 
- # # # #
-# # # #
- # # # #
-# # # #
- # # # #
-# # # #
- # # # #
-# # # #
-When you have a program that generates this pattern, define a variable size = 8 and change the program so that it works for any size, outputting a grid of the given width and height.
-*/
+		if (a > b ) {
+			result = b;
+		}
+		console.log(result);
+	}
 
-var boardSize = 12;
-var row = "";
+	min( 27,10);
 
-for (i = 0; i < boardSize; i++) {
-	
-	if (i % 2 === 0) { row += '#'; }  // start even rows with "#"
-
-	for (j = 0; row.length < boardSize -1; j++) {
-  	row += ' ';
-  	row += '#';
-  }
-
-  console.log(row + "\n");
-  row = "";
-  // console.log("i = " + i);
-
-}
+}());
