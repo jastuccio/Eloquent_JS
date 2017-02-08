7ee289370b46a225d36af72e9f134cb8793d06b6
joe <joe@astucc.io>
2017-02-07 22:45:12 -0500

    solve "number of Bs" bean counting

---
+++ 03_functions/bean_counting.js
@@ -0,0 +1,33 @@
+/*
+Bean counting
+
+You can get the Nth character, or letter, from a string by writing "string".charAt(N), similar to how you get its length with "s".length.
+The returned value will be a string containing only one character (for example, "b"). The first character has position zero, which causes the last one to 
+be found at position string.length - 1. In other words, a two-character string has length 2, and its characters have positions 0 and 1.
+
+Write a function countBs that takes a string as its only argument and returns a number that indicates how many uppercase “B” characters are in the string.
+
+Next, write a function called countChar that behaves like countBs, except it takes a second argument that indicates the character that is to be counted 
+(rather than counting only uppercase “B” characters). Rewrite countBs to make use of this new function.
+
+*/
+
+(function(){
+
+	var countBs = function (str) {
+	  var numOfB = 0;
+	  
+	  for ( i = 0; i < str.length - 1; i++) {
+	  	if (str.charAt(i) === "B") {
+	  		numOfB += 1;
+	  	}
+	  }
+
+	  return numOfB;
+	}
+
+	console.log(countBs("BBC"));
+
+	// console.log(countChar("kakkerlak", "k"));
+
+}());
