d9605a73156ca8783d57c506a5cb1f0685fedb4e
joe <joe@astucc.io>
2017-02-09 15:41:38 -0500

    Refactor bean_counting

--- 03_functions/bean_counting.js
+++ 03_functions/bean_counting.js
@@ -14,28 +14,20 @@ Next, write a function called countChar that behaves like countBs, except it tak
 
 (function(){
 
-	var countBs = function(str) {
-	  var numOfB = 0;
+	var countChar = function(str, ch) {
+	  var counted = 0;
 	  
 	  for ( i = 0; i < str.length; i++) {
-	  	if (str.charAt(i) === "B") {
-	  		numOfB += 1;
+	  	if (str.charAt(i) === ch) {
+	  		counted += 1;
 	  	}
 	  }
-
-	  return numOfB;
+	  return counted;
 	}
 
-	var countChar = function(strg, target) {
-	  var hits = 0;
-	  for ( i = 0; i < strg.length; i++) {
-	  	if (strg.charAt(i) === target) {
-	  		hits += 1;
-	  	}
-	  }
-      
-      return hits;
-	}
+	var countBs = function(str, target) {
+    return countChar(str, "B")
+  }
 
 	console.log("Bs = " + countBs("BBC"));
 	console.log("countChar = " + countChar("kakkerlak", "k"));
