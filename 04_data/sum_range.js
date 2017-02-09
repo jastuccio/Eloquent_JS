33a8ff9237c31418380baf8d390f43a98eef00f9
joe <joe@astucc.io>
2017-02-09 14:32:29 -0500

    refactor setting var step when it is undefined

--- 04_data/sum_range.js
+++ 04_data/sum_range.js
@@ -1,57 +1,42 @@
-/*
-The sum of a range
-
-The introduction of this book alluded to the following as a nice way to compute the sum of a range of numbers:
-
-console.log(sum(range(1, 10)));
-Write a range function that takes two arguments, start and end, and returns an array containing all the numbers from start up to (and including) end.
-
-Next, write a sum function that takes an array of numbers and returns the sum of these numbers. Run the previous program and see whether it does indeed return 55.
-
-As a bonus assignment, modify your range function to take an optional third argument that indicates the “step” value used to build up the array. If no step is given, the array elements go up by increments of one, corresponding to the old behavior. The function call range(1, 10, 2) should return [1, 3, 5, 7, 9]. Make sure it also works with negative step values so that range(5, 2, -1) produces [5, 4, 3, 2].
-*/
-
 (function() {
 
-    var range = function(start, end, step) {
-  	  //Don't use || to set default values in JavaScript
-  	  //codereadability.com/javascript-default-parameters-with-or-operator/
-    	   var arr = [];
+	var range = function(start, end, step) {
+		//Don't use || to set default values in JavaScript
+		//codereadability.com/javascript-default-parameters-with-or-operator/
+		var arr = [];
+		// console.log("step === undefined?" + step  );	
 
-    	   // for incremental arrays
-    	   if (start < end) {
-    	  	
-    	  	if (step === undefined) {											
-            step = 1;
-          }
-         
-          for (i = start; i <= end; i += step) {
-            arr.push(i);
-          }
-       
-        // for decremental arrays
-        } else {
-        	if (step === undefined) {											
-            step = -1;
-          }
-          for (i = start; i >= end; i += step ) {
-            arr.push(i);
-          }
-        }
-      return arr;
-    };
+		if (!step) {
+			step = start < end ? 1 : -1;
+		}
 
-	  var sum = function (arr) {
-		  var total = arr.reduce(function (prev, curr) {
-			  return prev + curr;
-			});
+		// for incremental arrays
+		if (start < end) {
 
-			return total;
-		};
+			for (i = start; i <= end; i += step) {
+				arr.push(i);
+			}
 
-    console.log(range(1, 10));
-    console.log("sum = " + sum(range(1, 10)));
-    console.log("expected '1, 3, 5, 7, 9' result: " + range(1, 10, 2));
-    console.log("expected '5, 4, 3, 2' result: " + range(5, 2, -1));
+			// for decremental arrays
+		} else {
+			for (i = start; i >= end; i += step) {
+				arr.push(i);
+			}
+		}
+		return arr;
+	};
 
-}());
+	var sum = function(arr) {
+		var total = arr.reduce(function(prev, curr) {
+			return prev + curr;
+		});
+
+		return total;
+	};
+
+	console.log("expected 1,2,3,4,5,6,7,8,9,10 \nresult: " + range(1, 10));
+	console.log("sum; expected 55 \nresult is: " + sum(range(1, 10)));
+	console.log("positive range by 2s; expected '1, 3, 5, 7, 9'\nresult: " + range(1, 10, 2));
+	console.log("negative range; expected '5, 4, 3, 2'\nresult: " + range(5, 2, -1));
+
+  }());
