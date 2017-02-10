c5237cce5db9b0af63fdc4440c7acb6bc6e0831e
joe <joe@astucc.io>
2017-02-09 20:04:45 -0500

    Add function nth

--- 04_data/list.js
+++ 04_data/list.js
@@ -27,8 +27,8 @@ If you haven’t already, also write a recursive version of nth.
 (function() {
   function arrayToList(arr) {
     var list = null;
-    for (var y = array.length -1; i >= 0; i--) {
-      list = {value: array[i], rest: list};
+    for (var i = arr.length -1; i >= 0; i--) {
+      list = {value: arr[i], rest: list};
     }
     return list;
   }
@@ -36,18 +36,28 @@ If you haven’t already, also write a recursive version of nth.
   function listToArray(list) {
     var arr = [];
     for (var node = list; node; node = node.rest) {
-      array.push(node.value);
+      arr.push(node.value);
     }
-    return array;
+    return arr;
+  }
+
+  function prepend(value, list) {
+    return {value: value, rest: list};
+  }
+
+  function nth(list, n) {
+    if (!list) { return undefined; }
+    else if (n == 0) { return list.value; }
+    else { return nth(list.rest, n -1); }
   }
 
 console.log(arrayToList([10, 20]));
+//→ {value: 10, rest: {value: 20, rest: null}}
+console.log(listToArray(arrayToList([10, 20, 30])));
+// → [10, 20, 30]
+console.log(prepend(10, prepend(20, null)));
 // → {value: 10, rest: {value: 20, rest: null}}
-// console.log(listToArray(arrayToList([10, 20, 30])));
-// // → [10, 20, 30]
-// console.log(prepend(10, prepend(20, null)));
-// // → {value: 10, rest: {value: 20, rest: null}}
-// console.log(nth(arrayToList([10, 20, 30]), 1));
-// // → 20
+console.log(nth(arrayToList([10, 20, 30]), 1));
+// → 20
 
   }());
